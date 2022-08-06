56bff064a98003a58aabb2c5a288b518c43c8251
Simon Martin <havvoric@gmail.com>
2022-08-06 21:15:35 +0100

    Day 21 complete.

---
+++ cyk/src/main.rs
@@ -0,0 +1,39 @@
+use anyhow::Result;
+use cyk::load;
+use env_logger::Env;
+use log::{error, info};
+use yansi::Paint;
+
+fn main() -> Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    let filename = "input/cyk.input";
+    let filename = "input/day19.input";
+    let mut solution = load(filename)?;
+    info!(
+        "{}{}: {:?}",
+        Paint::masked("🎄 "),
+        Paint::bold(Paint::yellow("solution")),
+        solution
+    );
+    solution.analyse();
+    match solution.answer_part1() {
+        Some(answer) => info!(
+            "{}part1 answer is {:?}",
+            Paint::masked("🎅 "),
+            Paint::bold(Paint::red(answer))
+        ),
+        _ => error!("{}No answer to part1", Paint::masked("🎅 ")),
+    }
+
+    match solution.answer_part2() {
+        Some(answer) => info!(
+            "{}part2 answer is {:?}",
+            Paint::masked("🎅 "),
+            Paint::bold(Paint::red(answer))
+        ),
+        _ => error!("{}No answer to part2", Paint::masked("🎅 ")),
+    }
+
+    Ok(())
+}
