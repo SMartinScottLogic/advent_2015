9f0f8a35b01818df7993c1fd53f0abfa704314fb
Simon Martin <smartin@scottlogic.com>
2022-08-20 22:36:20 +0100

    Day 23 complete (#24)
    Co-authored-by: Simon Martin <havvoric@gmail.com>

---
+++ day23/src/main.rs
@@ -0,0 +1,37 @@
+use anyhow::Result;
+use day23::load;
+use env_logger::Env;
+use log::{error, info};
+use yansi::Paint;
+
+fn main() -> Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    let mut solution = load("input/day23.input")?;
+    info!(
+        "{}{}: {:?}",
+        Paint::masked("🎄 "),
+        Paint::bold(Paint::yellow("solution")),
+        solution
+    );
+    solution.analyse();
+    match solution.answer_part1() {
+        Some(answer) => info!(
+            "{}part1 answer is {:?}",
+            Paint::masked("🎅 "),
+            Paint::bold(Paint::red(answer))
+        ),
+        _ => error!("{}No answer to part1", Paint::masked("🎅 ")),
+    }
+
+    match solution.answer_part2() {
+        Some(answer) => info!(
+            "{}part2 answer is {:?}",
+            Paint::masked("🎅 "),
+            Paint::bold(Paint::red(answer))
+        ),
+        _ => error!("{}No answer to part2", Paint::masked("🎅 ")),
+    }
+
+    Ok(())
+}
